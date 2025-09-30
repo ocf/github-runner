15a69744e736e133715eb297d67071cea19d5d3a
Joe Wang <zezhou_wang@berkeley.edu>
2025-09-29 18:49:39 -0700

    Add .transpire.py

---
+++ .transpire.py
@@ -0,0 +1,23 @@
+from transpire.resources import 
+from transpire.types import Image
+from transpire.utils import get_image_tag
+
+name = "github-runner"
+
+def objects():
+    yield Secret(
+        name=name,
+        string_data={
+            "RUNNER_TOKEN": "",
+        }
+    ).build()
+
+    deployment = Deployment(
+        name=name,
+        image=get_image_tag(name),
+    )
+    deployment.with_secret_env(name)
+    yield deployment.build()
+
+def images():
+    yield Image(name=name, path=Path("/"), registry="ghrc")
